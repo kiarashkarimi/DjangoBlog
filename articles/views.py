dd3b5d22822f257aca8475c174992fc22262f85c
kiarashkarimi <akiarash10@gmail.com>
2020-11-29 00:55:34 +0330

    urls

--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from . import models
 
 def articles_list(request):
     articles = models.Article.objects.all().order_by('date')
-
     args = {'articles': articles}
     return render(request, 'articles/articleslist.html', args)
+
+def articles_detail(request, slug):
+    return HttpResponse(slug)
